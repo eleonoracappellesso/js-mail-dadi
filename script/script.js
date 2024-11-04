76db90743b1e226b8f5267c0fd12b5f1d7835026
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-04 15:13:38 +0100

    created variables emails and emailUtente

---
+++ script/script.js
@@ -0,0 +1,19 @@
+"use strict";
+console.clear();
+
+/* 
+Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email
+*/
+
+const emails = [
+    "mail1@esempio.com",
+    "mail2@esempio.com",
+    "mail3@esempio.com",
+    "mail4@esempio.com"
+];
+
+const emailUtente = prompt("Inserisci qui la tua email:");

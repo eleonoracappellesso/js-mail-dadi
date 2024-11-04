b726ea017d6966f9af6207a2d613bbe28273fc2f
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-04 16:54:53 +0100

    used while to calculate exponents of 2

--- snack 5-6-7-9/script/script.js
+++ snack 5-6-7-9/script/script.js
@@ -13,10 +13,31 @@ const numeriDispari = [];
 for (let i = 0; i < 6; i++) {
     const numero = parseInt(prompt('Inserisci un numero:'));
     console.log(numero);
-// determino quali numeri sono dispari
+    // determino quali numeri sono dispari
     if (numero % 2) {
         numeriDispari.push(numero);
-    } 
+    }
 }
 console.log('Tra i numeri isneriti, i seguenti sono numeri dispari:', numeriDispari);
 
+/*
+JSnack 6
+Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente
+*/
+
+
+
+/*
+JSnack 7
+Stampa le potenze di 2 fino a 1000
+*/
+
+let potenza = 1;
+console.log('Le potenze di 2 fino a 1000 sono: ');
+
+while (potenza <= 1000) {
+    console.log(potenza);
+// calcolo la potenza
+    potenza *= 2;
+}

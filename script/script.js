b22a7520456adf9d77c9f9cbe7ec289e89247ea2
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-04 15:31:44 +0100

    added function to define a random number

--- script/script.js
+++ script/script.js
@@ -18,9 +18,23 @@ const emails = [
 const emailUtente = prompt("Inserisci qui la tua email:");
 console.log(emailUtente);
 
-
+// verifico se la mail è presente nell'array
 if (emails.includes(emailUtente)) {
     console.log("Accesso consentito!");
 } else {
     console.log("Accesso negato! Riprova con una mail differente.");
 }
+
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+// inserisco una funzione per generare casualmenre un numero da 1 a 6
+function numeroCasuale () {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+
+
